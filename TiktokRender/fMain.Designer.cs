581965a8994a6234464e821471418cab2660570c
thanh <thanh@THANHDB>
2023-03-04 17:17:40 +0700

    Add project files.

---
+++ TiktokRender/fMain.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace TiktokRender
+{
+    partial class fMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtVideo = new System.Windows.Forms.DataGridView();
+            this.SelectVideo = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Size = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Link = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnDownVideo = new System.Windows.Forms.Button();
+            this.btnGetVideo = new System.Windows.Forms.Button();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtVideo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(6, 5);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1266, 557);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.progressBar1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.dtVideo);
+            this.tabPage1.Controls.Add(this.btnDownVideo);
+            this.tabPage1.Controls.Add(this.btnGetVideo);
+            this.tabPage1.Controls.Add(this.txtUsername);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1258, 524);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Download video";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(964, 28);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(279, 29);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(619, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(525, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // dtVideo
+            // 
+            this.dtVideo.AllowUserToAddRows = false;
+            this.dtVideo.AllowUserToDeleteRows = false;
+            this.dtVideo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dtVideo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtVideo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.SelectVideo,
+            this.ID,
+            this.Title,
+            this.Time,
+            this.Size,
+            this.Link});
+            this.dtVideo.Location = new System.Drawing.Point(6, 85);
+            this.dtVideo.Name = "dtVideo";
+            this.dtVideo.RowHeadersWidth = 51;
+            this.dtVideo.RowTemplate.Height = 29;
+            this.dtVideo.Size = new System.Drawing.Size(1246, 433);
+            this.dtVideo.TabIndex = 2;
+            this.dtVideo.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtVideo_CellClick);
+            this.dtVideo.DoubleClick += new System.EventHandler(this.dtVideo_DoubleClick);
+            // 
+            // SelectVideo
+            // 
+            this.SelectVideo.HeaderText = "Chọn video";
+            this.SelectVideo.MinimumWidth = 6;
+            this.SelectVideo.Name = "SelectVideo";
+            this.SelectVideo.Width = 125;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.MinimumWidth = 6;
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            this.ID.Width = 200;
+            // 
+            // Title
+            // 
+            this.Title.HeaderText = "Tiêu đề";
+            this.Title.MinimumWidth = 6;
+            this.Title.Name = "Title";
+            this.Title.ReadOnly = true;
+            this.Title.Width = 600;
+            // 
+            // Time
+            // 
+            this.Time.HeaderText = "Thời gian";
+            this.Time.MinimumWidth = 6;
+            this.Time.Name = "Time";
+            this.Time.ReadOnly = true;
+            this.Time.Width = 125;
+            // 
+            // Size
+            // 
+            this.Size.HeaderText = "Kích thước";
+            this.Size.MinimumWidth = 6;
+            this.Size.Name = "Size";
+            this.Size.ReadOnly = true;
+            this.Size.Width = 125;
+            // 
+            // Link
+            // 
+            this.Link.HeaderText = "Link";
+            this.Link.MinimumWidth = 6;
+            this.Link.Name = "Link";
+            this.Link.Visible = false;
+            this.Link.Width = 125;
+            // 
+            // btnDownVideo
+            // 
+            this.btnDownVideo.Location = new System.Drawing.Point(384, 21);
+            this.btnDownVideo.Name = "btnDownVideo";
+            this.btnDownVideo.Size = new System.Drawing.Size(113, 58);
+            this.btnDownVideo.TabIndex = 1;
+            this.btnDownVideo.Text = "Down";
+            this.btnDownVideo.UseVisualStyleBackColor = true;
+            this.btnDownVideo.Click += new System.EventHandler(this.btnDownVideo_Click);
+            // 
+            // btnGetVideo
+            // 
+            this.btnGetVideo.Location = new System.Drawing.Point(220, 21);
+            this.btnGetVideo.Name = "btnGetVideo";
+            this.btnGetVideo.Size = new System.Drawing.Size(113, 58);
+            this.btnGetVideo.TabIndex = 1;
+            this.btnGetVideo.Text = "Get";
+            this.btnGetVideo.UseVisualStyleBackColor = true;
+            this.btnGetVideo.Click += new System.EventHandler(this.btnGetVideo_Click);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(26, 37);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(163, 27);
+            this.txtUsername.TabIndex = 0;
+            this.txtUsername.Text = "thanhdinhbao";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1258, 524);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Render video";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(701, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(169, 27);
+            this.textBox1.TabIndex = 5;
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1279, 567);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "fMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tiktok Renderer by ThanhDB v1.0";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtVideo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private Button btnDownVideo;
+        private Button btnGetVideo;
+        private TextBox txtUsername;
+        private TabPage tabPage2;
+        private DataGridView dtVideo;
+        private DataGridViewCheckBoxColumn SelectVideo;
+        private DataGridViewTextBoxColumn ID;
+        private DataGridViewTextBoxColumn Title;
+        private DataGridViewTextBoxColumn Time;
+        private DataGridViewTextBoxColumn Size;
+        private DataGridViewTextBoxColumn Link;
+        private Label label1;
+        private Label label2;
+        private ProgressBar progressBar1;
+        private TextBox textBox1;
+    }
+}
---
+++ TiktokRender/fMain.cs
@@ -0,0 +1,227 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System.ComponentModel;
+using System.Net;
+
+namespace TiktokRender
+{
+    public partial class fMain : Form
+    {
+        public fMain()
+        {
+            DataGridView.CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+        }
+        public string path;
+
+        #region ClassRespone
+
+        public class Author
+        {
+            public string id { get; set; }
+            public string unique_id { get; set; }
+            public string nickname { get; set; }
+            public string avatar { get; set; }
+        }
+
+        public class Data
+        {
+            public List<Video> videos { get; set; }
+            public string cursor { get; set; }
+            public bool hasMore { get; set; }
+        }
+
+        public class MusicInfo
+        {
+            public string id { get; set; }
+            public string title { get; set; }
+            public string play { get; set; }
+            public string cover { get; set; }
+            public string author { get; set; }
+            public bool original { get; set; }
+            public int duration { get; set; }
+            public string album { get; set; }
+        }
+
+        public class Root
+        {
+            public int code { get; set; }
+            public string msg { get; set; }
+            public double processed_time { get; set; }
+            public Data data { get; set; }
+        }
+
+        public class Video
+        {
+            public string video_id { get; set; }
+            public string region { get; set; }
+            public string title { get; set; }
+            public string cover { get; set; }
+            public string origin_cover { get; set; }
+            public int duration { get; set; }
+            public string play { get; set; }
+            public string wmplay { get; set; }
+            public int size { get; set; }
+            public int wm_size { get; set; }
+            public string music { get; set; }
+            public MusicInfo music_info { get; set; }
+            public int play_count { get; set; }
+            public int digg_count { get; set; }
+            public int comment_count { get; set; }
+            public int share_count { get; set; }
+            public int download_count { get; set; }
+            public int create_time { get; set; }
+            public Author author { get; set; }
+            public int is_top { get; set; }
+        }
+
+        #endregion
+
+
+        async void CallAPI(string cursor)
+        {
+            dtVideo.Rows.Clear();
+            dtVideo.Refresh();
+            string user_id = txtUsername.Text.Trim();
+            string url = "https://www.tikwm.com/api/user/posts?unique_id=" + user_id + "&count=35&cursor=" + cursor;
+            var client = new RestClient();
+            var request = new RestRequest(url, Method.Get);
+            RestResponse response = await client.ExecuteAsync(request);
+            var json = response.Content;
+            Root parse_json = JsonConvert.DeserializeObject<Root>(json);
+
+            int respone_code = parse_json.code;
+
+
+            if (respone_code == 0)
+            {
+                bool hasMore = parse_json.data.hasMore;
+                var currentDirectory = Directory.GetCurrentDirectory();
+                string path_user = parse_json.data.videos[0].author.unique_id;
+                path = currentDirectory + "\\" + path_user + "\\";
+
+                try
+                {
+                    for (int i = 0; i < parse_json.data.videos.Count + 1; i++)
+                    {
+
+                        dtVideo.Rows.Add(false, parse_json.data.videos[i].video_id, parse_json.data.videos[i].title, parse_json.data.videos[i].duration + " s", (parse_json.data.videos[i].size / 1000 + " kB"), parse_json.data.videos[i].play);
+                    }
+                }
+                catch (Exception)
+                {
+
+                }
+                Thread.Sleep(1000);
+
+                while (hasMore == true)
+                {
+                    Thread.Sleep(10000);
+                    CallAPI(parse_json.data.cursor);
+                    break;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng thử lại sau 10s!");
+            }
+
+
+        }
+
+        private void btnGetVideo_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text.Trim() == "")
+            {
+                MessageBox.Show("Chưa nhập username!");
+            }
+            else
+            {
+                Thread thread = new Thread(() =>
+                {
+                    CallAPI("");
+                });
+                thread.Start();
+            }
+        }
+
+        void DownloadVideo(string wmplay, string path)
+        {
+            var client = new WebClient();
+            client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+            client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+            client.DownloadFile(wmplay, path);
+
+        }
+
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                double bytesIn = double.Parse(e.BytesReceived.ToString());
+                double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+                double percentage = bytesIn / totalBytes * 100;
+                label1.Text += "Downloaded " + e.BytesReceived + " of " + e.TotalBytesToReceive;
+                textBox1.Text += "Percentage:" + percentage;
+                progressBar1.Value = int.Parse(Math.Truncate(percentage).ToString());
+            });
+        }
+        void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            this.BeginInvoke((MethodInvoker)delegate
+            {
+                textBox1.Text += "Completed";
+            });
+        }
+
+        private void dtVideo_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                dtVideo.Rows[e.RowIndex].Cells[0].Value = true;
+            }
+
+            dtVideo.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+
+        private void btnDownVideo_Click(object sender, EventArgs e)
+        {
+            int rowindex = dtVideo.CurrentCell.RowIndex;
+            string url = dtVideo.Rows[rowindex].Cells[5].Value.ToString();
+            string id = dtVideo.Rows[rowindex].Cells[1].Value.ToString();
+            string path_to_save = path + id + ".mp4";
+            bool check = (bool)dtVideo.Rows[rowindex].Cells[0].Value;
+
+            if (!File.Exists(path_to_save))
+            {
+                if (check == true)
+                {
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+
+                    DownloadVideo(url, path_to_save);
+
+                    MessageBox.Show("Down video thành công");
+                }
+                else
+                {
+                    MessageBox.Show("Chọn video để tải!!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Đã tải video!");
+            }
+
+        }
+
+        private void dtVideo_DoubleClick(object sender, EventArgs e)
+        {
+            int rowindex = dtVideo.CurrentCell.RowIndex;
+            dtVideo.Rows[rowindex].Cells[0].Value = false;
+
+        }
+    }
+}
